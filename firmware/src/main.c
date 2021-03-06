fea18ea60d6a52edb6a05f7607e4c30793e704d0
Matthew Sainsbury <matthew@sainsbury.io>
2021-03-05 18:45:46 -0800

    color wheel

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -1,20 +1,16 @@
 #include "main.h"
-
+#include "out.c"
 
 void app_main(void)
 {
-    unsigned int data[342] = {0};
-
     setup_leds();
 
     while (1)
     {
-        for (int i = 0; i < 342; i++)
+        for (int i = 0; i < 36; i++)
         {
-            data[i] = 0x010000;
-            if (i > 0) data[i-1] = 0;
-            set_strip(data);
-            vTaskDelay(90 / portTICK_PERIOD_MS);
+            set_strip(data[i]);
+            vTaskDelay(25 / portTICK_PERIOD_MS);
         }
     }
 }
