ac3561eb1dcdd11cd7732dce4b612d08bc1ddf13
Matthew Sainsbury <matthew@sainbury.io>
2021-03-13 16:24:38 -0800

    improved client code

--- client/app.tsx
+++ client/app.tsx
@@ -1,10 +1,10 @@
-import React, { useEffect, useRef } from 'react';
-import { render } from 'react-dom';
+import React, { useRef } from 'react';
 import Canvas from './canvas';
 
 let webSocket = new WebSocket("ws://192.168.0.198");
 
 let pixels = new Uint32Array(513).fill(0);
+let newPixels = new Uint32Array(513).fill(0);
 
 export default () => {
     let i = 0;
@@ -12,24 +12,20 @@ export default () => {
 
     let ref = useRef<Canvas>(null);
 
-
-    const setPixel = (x: number, y: number, color: number) => {
-        pixels[513 - x - y * 19] = color;
-        ref.current.setPixel(x, y, color);
-    }
-
-    (document as any).setPixel = setPixel;
-
-    setInterval(() => {
-        setPixel(i, j, 0xff);
+    setTimeout(() => {
+        ref.current.setPixel(i, j, 0xff);
+        ref.current.setPixel(i+1, j, 0xff00);
+        ref.current.setPixel(i, j+1, 0xff0000);
+        
         i++;
         if (i == 19) {
             j++;
             i = 0;
         }
+        for (let m = 0; m< 10; m++) ref.current.setPixel(Math.floor(Math.random()*19), Math.floor(Math.random()*27), Math.floor(Math.random()*0xffffff));
 
-        webSocket.send(pixels);
-    }, 100);
+        webSocket.send(ref.current.getImageData());
+    }, 500);
 
     return (<Canvas ref={ref} />);
 }
--- client/canvas.tsx
+++ client/canvas.tsx
@@ -1,14 +1,12 @@
 import * as React from 'react';
 
 export default class extends React.Component {
-    scaling = 20;
-    actualSize = [19, 27];
+    size = [19, 27];
     canvasRef = React.createRef<HTMLCanvasElement>();
     ctx: CanvasRenderingContext2D;
 
     componentDidMount() {
         this.ctx = (this.canvasRef.current as HTMLCanvasElement).getContext('2d');
-        this.ctx.scale(this.scaling, this.scaling);
     }
 
     public setPixel(x: number, y: number, color: number) {
@@ -16,7 +14,21 @@ export default class extends React.Component {
         this.ctx.fillRect(x, y, 1, 1);
     }
 
+    public getImageData() {
+        const imgData = this.ctx.getImageData(0, 0, this.size[0], this.size[1]).data;
+        const result = new Uint32Array(this.size[0] * this.size[1]).fill(0);
+
+        for(let i = 0; i < this.size[0] * this.size[1]; i++) {
+                const position = i * 4;
+                const red = Math.floor(imgData[position] / 5);
+                const green = Math.floor(imgData[position + 1] / 5);
+                const blue = Math.floor(imgData[position + 2] / 5);
+                result[i] = (red << 16) | (green << 8) | (blue);
+        }
+        return result.reverse();
+    }
+
     render() {
-        return (<canvas width={this.actualSize[0] * this.scaling} height={this.actualSize[1] * this.scaling} ref={this.canvasRef} />);
+        return (<canvas width={this.size[0]} height={this.size[1]} ref={this.canvasRef} />);
     }
 }
