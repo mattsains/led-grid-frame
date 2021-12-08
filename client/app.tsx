71224adb46f8d8012882aad528c7987e1737f333
Matthew Sainsbury <matthew@sainbury.io>
2021-12-07 19:29:58 -0800

    it works!

--- client/app.tsx
+++ client/app.tsx
@@ -1,23 +1,31 @@
 import React, { useRef } from 'react';
 import Canvas from './canvas';
 
-let webSocket = new WebSocket("ws://192.168.0.198");
+let webSocket = new WebSocket("ws://192.168.0.199");
 
 export default () => {
-    let rate = 50;
+    let rate = 10;
     let ref = useRef<Canvas>(null);
     let ctx;
     class T {
-        x =  0;
-        y = 0;
-        vx = rate/40;
-        vy = rate/40;
+        constructor(x, y, red, green, blue, vx, vy) {
+            this.x = x;
+            this.y = y;
+            this.red = red;
+            this.green = green;
+            this.blue = blue;
+            this.vx = vx
+            this.vy = vy;
+        }
+        x;
+        y;
+        red;
+        green;
+        blue;
+        vx;
+        vy;
         radius = 4;
-        red = 0;
-        green = 0;
-        blue = 255;
         draw() {
-            ctx.clearRect(0,0, 19, 27);
             ctx.beginPath();
             ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
             ctx.closePath();
@@ -26,8 +34,20 @@ export default () => {
             ctx.fill();
         }
       }
-    let ball = new T();
+    let balls = [
+        new T(0, 0, 0, 255, 255, rate/40, rate/40), 
+        new T(0, 0, 0, 255, 0, rate/30, rate/30),
+        new T(0, 0, 255, 255, 0, rate/35, rate/35),
+        new T(0, 0, 255, 0, 255, rate/20, rate/20),
+        new T(0, 0, 255, 255, 255, rate/45, rate/40), 
+        new T(0, 0, 255, 255, 0, rate/35, rate/30),
+        new T(0, 0, 120, 255, 0, rate/35, rate/35),
+        new T(0, 0, 120, 0, 255, rate/25, rate/10),
+        new T(0, 0, 255, 0, 0, rate/50, rate/50)
+    ];
     function draw() {
+        ctx.clearRect(0,0, 19, 27);
+        for (let ball of balls) {
         ball.draw();
         ball.x += ball.vx;
         ball.y += ball.vy;
@@ -37,24 +57,19 @@ export default () => {
         if (ball.x + ball.vx > 19 || ball.x + ball.vx < 0) {
             ball.vx = -ball.vx;
         }
-        ball.red = Math.max(0, Math.min(255, Math.round(ball.red + 20*(Math.random() - 0.5))));
-        ball.green = Math.max(0, Math.min(255, Math.round(ball.green + 20*(Math.random() - 0.5))))
-        ball.blue = Math.max(0, Math.min(255, Math.round(ball.blue + 20*(Math.random() - 0.5))))
+        ball.red = Math.max(0, Math.min(255, Math.round(ball.red + (Math.random() - 0.5))));
+        ball.green = Math.max(0, Math.min(255, Math.round(ball.green + (Math.random() - 0.5))))
+        ball.blue = Math.max(0, Math.min(255, Math.round(ball.blue + (Math.random() - 0.5))))
       }
-    setTimeout(() => {
-        ctx = ref.current?.ctx;
-      
-        if (ctx) ball.draw();
-    }, rate * 10);
+    }
 
     setInterval(() => {
-        if (ctx) draw();
-        if (webSocket.readyState == webSocket.CLOSED || webSocket.readyState == webSocket.CLOSING)
-            webSocket = new WebSocket("ws://192.168.0.198");
-        else
+        if (!ctx) ctx = ref.current?.ctx;
+        else {
+            draw();
             webSocket.send(ref.current.getImageData());
-
-    }, rate);
+        }
+    }, 16); //16 - 60fps
 
     return (<Canvas ref={ref} />);
 }
