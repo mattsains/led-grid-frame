a0cf1fcc08ba15693e7238a332e2e8b6b254d952
Matthew Sainsbury <matthew@sainbury.io>
2021-12-12 14:34:43 -0800

    WIP tetris

--- client/app.tsx
+++ client/app.tsx
@@ -1,36 +1,37 @@
-import React, { useRef } from 'react';
-import Canvas from './canvas';
+import React, { useRef } from "react";
+import Canvas from "./canvas";
+
+import "./tetris";
+import tetris from "./tetris";
 
 let webSocket = new WebSocket("ws://192.168.0.199");
 declare var FontFace;
-const myFont = new FontFace('FreePixel', 'url(FreePixel.ttf)');
+const myFont = new FontFace("FreePixel", "url(FreePixel.ttf)");
 (document as any).fonts.add(myFont);
 
 export default () => {
-    let ref = useRef<Canvas>(null);
-    let canvas: Canvas;
+  let ref = useRef<Canvas>(null);
+  let canvas: Canvas;
 
-    let scroll = 0;
-    let text = "😀🍆🍑😘👌💕🤦‍♀️😎🎶😍🤐😰🤬🤠😈👺"
+  function update() {
+    tetris.update();
+  }
 
-   function draw() {
-        canvas.ctx.clearRect(0,0, 19, 27);
-        canvas.ctx.fillStyle = "white";
-        canvas.ctx.imageSmoothingEnabled = false;
-        canvas.ctx.font = "20pt FreePixel";
-        canvas.ctx.fillText(text, 19 - Math.floor(scroll), 23);
-        scroll += 0.6;
-        if (scroll > canvas.ctx.measureText(text).width + 19) scroll = 0;
+  function draw() {
+    canvas.ctx.clearRect(0, 0, 19, 27);
+    tetris.draw(canvas.ctx);
+  }
+
+  setInterval(() => update(), 100);
+  setInterval(() => {
+    if (canvas == undefined) {
+      canvas = ref.current;
+    } else {
+      draw();
+      if (webSocket.readyState == WebSocket.OPEN)
+        webSocket.send(canvas.getImageData());
     }
+  }, 16); //16 - 60fps
 
-    setInterval(() => {
-        if (canvas == undefined) {
-            canvas = ref.current;
-        } else {
-            draw();
-            if (webSocket.readyState == WebSocket.OPEN) webSocket.send(canvas.getImageData());
-        }
-    }, 16); //16 - 60fps
-
-    return (<Canvas ref={ref} />);
-}
+  return <Canvas ref={ref} />;
+};
